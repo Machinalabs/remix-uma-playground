cec8ae77cb5575e99a646aa922c6f655511ae9da
Edson Alcala <edson_alcala@msn.com>
2021-02-12 22:18:27 +0000

    added types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,13 @@
+import { ethers } from "ethers";
+
 export type Bytes20 = string
 
 export type EthereumAddress = Bytes20
+
+export type TimeStamp = string;
+
+export type Web3Provider = ethers.providers.Web3Provider;
+
+export type Signer = ethers.Signer;
+
+export type Block = ethers.providers.Block;
